b727061c81cc598b416e17c278e130d426a540f1
James Bramich <james.bramich@gmail.com>
2022-02-08 16:47:07 +1100

    renamed payouts/post

--- payouts/payouts_post.js
+++ payouts/payouts_post.js
@@ -1,5 +1,7 @@
+// This script creates a payout request.
+
 // These modules allow the code to run on Node.js, they aren't required if running in a browser.
-const fetch = require("cross-fetch");
+const fetch = require('cross-fetch');
 const FormData = require('form-data');
 
 // Remember, the JWT access token must be securely store - this example uses an environment variable
@@ -7,25 +9,28 @@ const jwtToken = process.env.NOFRIXION_SANDBOX_TOKEN;
 
 const url = 'https://api-sandbox.nofrixion.com/api/v1/payouts';
 
-// Build payoutData object.
-let payoutData = new FormData();
-payoutData.append("AccountID", "A120P0JR");
-payoutData.append("Currency", "EUR");
-payoutData.append("Amount", "7.89");
-payoutData.append("YourReference", "My reference");
-payoutData.append("DestinationIBAN", "GB94BARC10201530093459");
-payoutData.append("DestinationAccountName", "Destination Name");
-payoutData.append("TheirReference", "Their reference");
+// Example form data
+const form = new FormData();
+form.append("AccountID", "A120P0JR");
+form.append("Currency", "EUR");
+form.append("Amount", "0.01");
+form.append("YourReference", "Sender ref");
+form.append("DestinationIBAN", "GB33BUKB20201555555555");
+form.append("DestinationAccountName", "Test Account");
+form.append("TheirReference", "Recipient ref");
 
 const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-        Accept: "application/text",
-        Authorization: "Bearer " + jwtToken
-    },
-    body: payoutData
+        Accept: 'text/plain',
+        Authorization: 'Bearer ' + jwtToken
+    }
 };
 
+options.body = form;
+
 fetch(url, options)
-    .then(response => console.log(response.status)) //Expect '200' for success.
+    .then(response => response.text())
+    // the response text is the payout ID of the newly created payout.
+    .then(response => console.log(response))
     .catch(err => console.error(err));
