2feea9cd9dd117cc20a2293d199fe2c2ab0d30ae
James Bramich <james.bramich@gmail.com>
2022-02-28 14:16:28 +1100

    updated for paging

--- payouts/payouts_get.js
+++ payouts/payouts_get.js
@@ -6,24 +6,24 @@
 // 1. Create a user access token in the sandbox portal at:
 //    https://portal-sandbox.nofrixion.com.
 // 2. Set the token as an environment variable in your console:
-//    set NOFRIXION_SANDBOX_TOKEN=<JWT token from previous step>
+//    set NOFRIXION_USER_TOKEN=<JWT token from previous step>
 // 3. Run the script in a browser or using node.js
-// 4. If successful user a list of payouts will be displayed as an array of JSON objects.
+// 4. If successful a JSON object containing a page of payouts will be displayed.
 //-----------------------------------------------------------------------------
 
 // This module allows the code to run on Node.js, it's not required if running in a browser.
-const fetch = require("cross-fetch");
+const fetch = require('cross-fetch');
 
 // Remember, the JWT access token must be securely store - this example uses an environment variable
-const jwtToken = process.env.NOFRIXION_SANDBOX_TOKEN;
+const jwtToken = process.env.NOFRIXION_USER_TOKEN;
 
 const url = 'https://api-sandbox.nofrixion.com/api/v1/payouts';
 
 const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-        Accept: "application/json",
-        Authorization: "Bearer " + jwtToken
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + jwtToken
     }
 };
 
