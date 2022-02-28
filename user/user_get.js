fda38d527985603fc2ecb53c9d38e38b41d91669
James Bramich <james.bramich@gmail.com>
2022-02-28 13:53:36 +1100

    updated to accept application/json

--- user/user_get.js
+++ user/user_get.js
@@ -20,7 +20,7 @@ const jwtToken = process.env.NOFRIXION_USER_TOKEN;
 
 const baseUrl = 'https://api-sandbox.nofrixion.com/api/v1/user';
 
-const options = { method: 'GET', headers: { Accept: 'text/plain', Authorization: 'Bearer ' + jwtToken } };
+const options = { method: 'GET', headers: { Accept: 'application/json', Authorization: 'Bearer ' + jwtToken } };
 
 fetch(baseUrl, options)
     .then(response => response.json())
