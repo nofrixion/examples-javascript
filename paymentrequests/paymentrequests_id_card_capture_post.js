aff86b8aef9e6e7dd73c2655ab91f9fab20e6eb9
James Bramich <james.bramich@gmail.com>
2022-03-07 14:47:43 +1100

    updated for card payments

--- paymentrequests/paymentrequests_id_card_capture_post.js
+++ paymentrequests/paymentrequests_id_card_capture_post.js
@@ -1,6 +1,7 @@
 //-----------------------------------------------------------------------------
-// Description: Example of calling the NoFrixion MoneyMoov API paymentrequests/{id} 
-// GET method. It returns the details of the specified payment request.
+// Description: Example of calling the NoFrixion MoneyMoov API 
+// paymentrequests/{id}/card/capture POST method. It captures payment for a   
+// previously authorised card payment.
 //
 // Usage:
 // 1. Create a MERCHANT access token in the sandbox portal at:
@@ -8,29 +9,36 @@
 // 2. Set the token as an environment variable in your console:
 //    set NOFRIXION_MERCHANT_TOKEN=<JWT token from previous step>
 // 3. Run the script in a browser or using node.js
-// 4. If successful the JSON object containing the payment request data will be displayed.
+// 4. If successful JSON object containing the card payment respone
+//    model will be displayed
 //-----------------------------------------------------------------------------
 
 // These modules allow the code to run on Node.js, they aren't required if running in a browser.
 const fetch = require('cross-fetch');
+const FormData = require('form-data');
 
 // Remember, the JWT access token must be securely store - this example uses an environment variable
 const jwtToken = process.env.NOFRIXION_MERCHANT_TOKEN;
 
 const baseUrl = 'https://api-sandbox.nofrixion.com/api/v1/paymentrequests';
 
-const options = {
-    method: 'GET',
+var paymentRequestID = 'c97e3530-812f-45b8-11fc-08d9f4f51c4a';
+
+// Example form data
+var form = new FormData();
+form.append('amount', '0.50');
+
+var options = {
+    method: 'POST',
     headers: {
         Accept: 'application/json',
         Authorization: 'Bearer ' + jwtToken
-    }
+    },
+    body: form
 };
 
-var paymentRequestID = '1744b1f6-c92f-43ce-9889-08d9f65a6611';
-
-fetch(`${baseUrl}/${paymentRequestID}/result`, options)
+fetch(`${baseUrl}/${paymentRequestID}/card/capture`, options)
     .then(response => response.json())
-    // the response JSON contains the payment request details.
+    // Card payment response model will be returned in JSON response object.
     .then(responseJson => console.log(responseJson))
     .catch(err => console.error(err));
--- paymentrequests/paymentrequests_id_card_void_post.js
+++ paymentrequests/paymentrequests_id_card_void_post.js
@@ -1,6 +1,7 @@
 //-----------------------------------------------------------------------------
-// Description: Example of calling the NoFrixion MoneyMoov API paymentrequests/{id} 
-// GET method. It returns the details of the specified payment request.
+// Description: Example of calling the NoFrixion MoneyMoov API 
+// paymentrequests/{id}/card/void POST method. It voids a recently  
+// processed card payment, authorisation or capture.
 //
 // Usage:
 // 1. Create a MERCHANT access token in the sandbox portal at:
@@ -8,29 +9,31 @@
 // 2. Set the token as an environment variable in your console:
 //    set NOFRIXION_MERCHANT_TOKEN=<JWT token from previous step>
 // 3. Run the script in a browser or using node.js
-// 4. If successful the JSON object containing the payment request data will be displayed.
+// 4. If successful JSON object containing the card payment respone
+//    model will be displayed.
 //-----------------------------------------------------------------------------
 
 // These modules allow the code to run on Node.js, they aren't required if running in a browser.
 const fetch = require('cross-fetch');
+const FormData = require('form-data');
 
 // Remember, the JWT access token must be securely store - this example uses an environment variable
 const jwtToken = process.env.NOFRIXION_MERCHANT_TOKEN;
 
 const baseUrl = 'https://api-sandbox.nofrixion.com/api/v1/paymentrequests';
 
-const options = {
-    method: 'GET',
+var paymentRequestID = '1744b1f6-c92f-43ce-9889-08d9f65a6611';
+
+var options = {
+    method: 'POST',
     headers: {
         Accept: 'application/json',
         Authorization: 'Bearer ' + jwtToken
     }
 };
 
-var paymentRequestID = '1744b1f6-c92f-43ce-9889-08d9f65a6611';
-
-fetch(`${baseUrl}/${paymentRequestID}/result`, options)
+fetch(`${baseUrl}/${paymentRequestID}/card/void`, options)
     .then(response => response.json())
-    // the response JSON contains the payment request details.
+    // Card payment response model will be returned in JSON response object.
     .then(responseJson => console.log(responseJson))
     .catch(err => console.error(err));
